088d11f09ca59d1195503d1a5b8c1256dec67b0b
Tamir Ashkenazy <tamir.ashkenazy@live.biu.ac.il>
2019-05-29 09:05:38 +0300

    Add project files.

---
+++ WebApplication2/Controllers/FirstController.cs
@@ -0,0 +1,82 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Script.Services;
+using System.Web.Services;
+using System.Xml;
+using WebApplication2.Models;
+
+namespace WebApplication2.Controllers
+{
+    public class FirstController : Controller
+    {
+        private static Random rnd = new Random();
+
+        // GET: First
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult display(string ip, int port, int time)
+        {
+            //מחלקה ששומרת את הנתונים ומנהלת אותם
+            InfoModel.Instance.ip = ip;
+            InfoModel.Instance.port = port.ToString();
+            InfoModel.Instance.time = time;
+
+            InfoModel.Instance.ReadData("Dor");
+
+            Session["time"] = time;
+
+
+            return View();
+        }
+
+
+        [HttpPost]
+        //כי פנינו אליה בפוסט, אז כותבים עליה פוסט למעלה
+        public string GetEmployee()
+        {
+            var emp = InfoModel.Instance.Employee;
+
+            emp.Salary = rnd.Next(1000);
+            //התשובה שמוחזרת היא
+            return ToXml(emp);
+        }
+
+        private string ToXml(Employee employee)
+        {
+            //Initiate XML stuff
+            StringBuilder sb = new StringBuilder();
+            XmlWriterSettings settings = new XmlWriterSettings();
+            XmlWriter writer = XmlWriter.Create(sb, settings);
+
+            writer.WriteStartDocument();
+            writer.WriteStartElement("Employess");
+
+            employee.ToXml(writer);
+
+            writer.WriteEndElement();
+            writer.WriteEndDocument();
+            writer.Flush();
+            return sb.ToString();
+        }
+
+
+        // POST: First/Search
+        [HttpPost]
+        public string Search(string name)
+        {
+            InfoModel.Instance.ReadData(name);
+
+            return ToXml(InfoModel.Instance.Employee);
+        }
+
+    }
+}
---
+++ WebApplication2/Models/Employee.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.Xml;
+
+namespace WebApplication2.Models
+{
+    public class Employee
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Salary { get; set; }
+
+        public void ToXml(XmlWriter writer)
+        {
+            writer.WriteStartElement("Employee");
+            writer.WriteElementString("FirstName", this.FirstName);
+            writer.WriteElementString("LastName", this.LastName);
+            writer.WriteElementString("Salary", this.Salary.ToString());
+            writer.WriteEndElement();
+        }
+    }
+}
